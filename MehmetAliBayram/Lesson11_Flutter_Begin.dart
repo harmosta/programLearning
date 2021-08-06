ec5b9a305083f32a2761ea75d49f9eef0ab4e49e
Harmosta <ramazanacar@outlook.com.tr>
2021-08-06 15:38:08 +0300

    Application name changed to Task List

--- MehmetAliBayram/Lesson11_Flutter_Begin.dart
+++ MehmetAliBayram/Lesson11_Flutter_Begin.dart
@@ -2,39 +2,39 @@ import 'package:flutter/material.dart';
 
 //import 'package:flutter/src/material/floating_action_button_location.dart';
 
-void islem() {
-  print("Islem Gerceklesti.");
-}
+// void islem() {
+ // print("Islem Gerceklesti.");
+// }
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.air),
-          title: Text("Flutter Uygulama"),
+          leading: Icon(Icons.shopping_cart_sharp),
+          title: Text("Task List"),
           backgroundColor: Colors.pink,
         ),
         body: Text("Burasi Sayfanin Ic Kismidir"),
         // body: Text: ("11. video ders icerigidir."),
         // floatingActionButtonAnimator: FloatingActionButtonAnimator(),
-        floatingActionButton: FloatingActionButton(
-          //mini: true,
-          backgroundColor: Colors.pink,
-          onPressed: islem,
-          child: Icon(Icons.beach_access_sharp),
-          // FloatingActionButtonLocation.centerFloat,
-        ),
+        // floatingActionButton: FloatingActionButton(
+           // mini: true,
+           // backgroundColor: Colors.pink,
+           // onPressed: islem,
+           // child: Icon(Icons.beach_access_sharp),
+           // FloatingActionButtonLocation.centerFloat,
+        // ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
+           // BottomNavigationBarItem(
+            //  icon: Icon(Icons.home),
+            //  label: 'Home',
+            //  backgroundColor: Colors.purple,
+           // ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-              backgroundColor: Colors.purple,
-            ),
-            BottomNavigationBarItem(
-              icon:Icon(Icons.view_list_sharp),
-              label:'List',
+              icon: Icon(Icons.view_list_sharp),
+              label: 'List',
               backgroundColor: Colors.red,
             ),
           ],
