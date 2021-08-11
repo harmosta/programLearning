1b3629b3d5f4a30c47243139f8458973f74f81bd
Harmosta <ramazanacar@outlook.com.tr>
2021-08-11 13:10:47 +0300

    ListTile example added inside Lesson13 and some files deleted.

---
+++ test/test.dart
@@ -0,0 +1,27 @@
+
+Function islem1 = (){
+print('islem1 gerceklestirildi.');
+};
+
+void main(){
+List <String> sehirler = ['Izmir', 'Adana', 'Antalya', 'Manisa'];
+
+print (sehirler);
+print (sehirler[2]);
+print (sehirler.length);
+
+sehirler.add('Tekirdag');
+
+print(sehirler);
+print(sehirler[4]);
+
+int index=sehirler.indexOf('Manisa');
+print(index);
+
+int yas= 19;
+String sonuc = yas >= 18 ? 'Resitsiniz.' : 'Resit Degilsiniz.';
+print (sonuc);
+
+islem1.call();
+print('Islem2 gerceklesti.');
+}
