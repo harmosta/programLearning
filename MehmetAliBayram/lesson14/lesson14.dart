3c07f622d5cfb1e586ba18e3b6b552358b479ca8
Harmosta <ramazanacar@outlook.com.tr>
2021-08-13 10:15:25 +0300

    An example started about if condition in lesson14.dart

---
+++ MehmetAliBayram/lesson14/lesson14.dart
@@ -0,0 +1,10 @@
+void main(){
+
+String kullaniciAdi = "Mehmet";
+
+if (kullaniciAdi.length < 10)
+print ("Kullanici adi 10 karakterden kisa olamaz.");
+
+print ("Lutfen 10 karakterden uzun kullanici adi giriniz.");
+
+}
