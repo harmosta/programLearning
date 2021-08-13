923387c185e6b8f11afb2d5a69b188036bde5fce
Harmosta <ramazanacar@outlook.com.tr>
2021-08-13 11:52:21 +0300

    Example expanded with  IfElse command.

--- MehmetAliBayram/lesson14/lesson14.dart
+++ MehmetAliBayram/lesson14/lesson14.dart
@@ -1,15 +1,16 @@
 //import 'package:flutter/material.dart';
 
-void main (){
-  
- String kullaniciAdi = "MehaetfSF544a4f6AD4F";
-  
- if (kullaniciAdi.contains ("m") || kullaniciAdi.contains ("M"))
- {
-  print ("Kullanici adi m harfi icermemelidir.");
+void main() {
+  String kullaniciAdi = "hafSF544a4f6AD4F";
+
+  if (kullaniciAdi.contains("m") || kullaniciAdi.contains("M")) {
+    print("Kullanici adi m harfi icermemelidir.");
     // print ("Lutfen 10 karakterden uzun kullanici adi giriniz.");
- }
-  else {
-    print ("Tebrikler kullanici adini m harfini icermemektedir.");
+  } else if (kullaniciAdi.contains("t") || kullaniciAdi.contains("T")) {
+    print("Kullanici adi t harfi icermemelidir.");
+  } else if (kullaniciAdi.contains("e") || kullaniciAdi.contains("E")) {
+    print("Kullanici adi e harfi icermemelidir.");
+  } else {
+    print("Tebrikler kullanici adininiz m, t, e harflerini icermemektedir.");
   }
 }
