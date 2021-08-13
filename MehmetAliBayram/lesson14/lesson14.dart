06aab9e5baac13ba28f84f49b66b91ea685539a9
Harmosta <ramazanacar@outlook.com.tr>
2021-08-13 13:10:12 +0300

    else if example added.

--- MehmetAliBayram/lesson14/lesson14.dart
+++ MehmetAliBayram/lesson14/lesson14.dart
@@ -6,15 +6,36 @@ void main() {
   if (kullaniciAdi.contains("m") || kullaniciAdi.contains("M")) {
     print("Kullanici adi m harfi icermemelidir.");
     // print ("Lutfen 10 karakterden uzun kullanici adi giriniz.");
-  } 
-  else if (kullaniciAdi.contains("t") || kullaniciAdi.contains("T")) {
+  } else if (kullaniciAdi.contains("t") || kullaniciAdi.contains("T")) {
     print("Kullanici adi t harfi icermemelidir.");
-  }
-  
-  else if (kullaniciAdi.contains("e") || kullaniciAdi.contains("E")) {
+  } else if (kullaniciAdi.contains("e") || kullaniciAdi.contains("E")) {
     print("Kullanici adi e harfi icermemelidir.");
-  } 
-  else {
+  } else {
     print("Tebrikler kullanici adininiz m, t, e harflerini icermemektedir.");
   }
+
+  int abrt = 35;
+
+  if (abrt == 0)
+    print("Aklinizdan tuttugunuz rakam 0 dir.");
+  else if (abrt == 1)
+    print("Aklinizdan tuttugunuz rakam 1 dir.");
+  else if (abrt == 2)
+    print("Aklinizdan tuttugunuz rakam 2 dir.");
+  else if (abrt == 3)
+    print("Aklinizdan tuttugunuz rakam 3 dir.");
+  else if (abrt == 4)
+    print("Aklinizdan tuttugunuz rakam 4 dir.");
+  else if (abrt == 5)
+    print("Aklinizdan tuttugunuz rakam 5 dir.");
+  else if (abrt == 6) 
+    print("Aklinizdan tuttugunuz rakam 6 dir.");
+  else if (abrt == 7)
+    print("Aklinizdan tuttugunuz rakam 7 dir.");
+  else if (abrt == 8)
+    print("Aklinizdan tuttugunuz rakam 8 dir.");
+  else if (abrt == 9)
+    print("Aklinizdan tuttugunuz rakam 9 dir.");
+  else
+    print ("Aklinizdaki rakami bulamadim.");
 }
