76dc9186d9b072616f071e6d6c916a77f8e4c375
Harmosta <ramazanacar@outlook.com.tr>
2021-08-13 12:05:53 +0300

    Lesson14 example made logical

--- MehmetAliBayram/lesson14/lesson14.dart
+++ MehmetAliBayram/lesson14/lesson14.dart
@@ -1,16 +1,19 @@
 //import 'package:flutter/material.dart';
 
 void main() {
-  String kullaniciAdi = "hafSF544a4f6AD4F";
+  String kullaniciAdi = "Mehme hafSF544a4f6AD4F";
 
   if (kullaniciAdi.contains("m") || kullaniciAdi.contains("M")) {
     print("Kullanici adi m harfi icermemelidir.");
     // print ("Lutfen 10 karakterden uzun kullanici adi giriniz.");
-  } else if (kullaniciAdi.contains("t") || kullaniciAdi.contains("T")) {
+  } 
+  if (kullaniciAdi.contains("t") || kullaniciAdi.contains("T")) {
     print("Kullanici adi t harfi icermemelidir.");
-  } else if (kullaniciAdi.contains("e") || kullaniciAdi.contains("E")) {
+  } 
+  if (kullaniciAdi.contains("e") || kullaniciAdi.contains("E")) {
     print("Kullanici adi e harfi icermemelidir.");
-  } else {
+  } 
+  else {
     print("Tebrikler kullanici adininiz m, t, e harflerini icermemektedir.");
   }
 }
---
+++ test/ifExample.dart
@@ -0,0 +1,12 @@
+//import 'package:flutter/material.dart';
+
+void main (){
+  
+ String kullaniciAdi = "MehmetfSF544a4f6AD4F";
+  
+ if (kullaniciAdi.contains ("m"))
+ {
+  print ("Kullanici adi m harfi icermemelidir.");
+ // print ("Lutfen 10 karakterden uzun kullanici adi giriniz.");
+ }
+}
